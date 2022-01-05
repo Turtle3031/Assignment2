8dd597d93b1ca84d4862a17c858a24d696a7a06f
Turtle3031 <97133874+Turtle3031@users.noreply.github.com>
2022-01-05 10:12:59 +1000

    Update webtest.py

--- webtest.py
+++ webtest.py
@@ -20,3 +20,16 @@ c = alt.Chart(df).mark_circle().encode(
 
 st.write(c)
 
+import pandas as pd
+import streamlit as st
+
+st.title("Welcome to Streamlit!")
+
+st.write("Our first DataFrame")
+
+st.write(
+  pd.DataFrame({
+      'A': [1, 2, 3, 4],
+      'B': [5, 6, 7, 8]
+    })
+)
