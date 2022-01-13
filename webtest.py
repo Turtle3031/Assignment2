c9ae02ab1874a309aaa93da318efe507d841b28d
Turtle3031 <97133874+Turtle3031@users.noreply.github.com>
2022-01-13 12:37:32 +1000

    Add files via upload

--- webtest.py
+++ webtest.py
@@ -33,3 +33,17 @@ st.write(
       'B': [5, 6, 7, 8]
     })
 )
+
+import pandas as pd
+import streamlit as st
+
+st.title("Welcome to Streamlit!")
+
+st.write("Line Chart in Streamlit")
+# 10 * 2 dimensional data
+chart_data = pd.DataFrame(
+    np.random.randn(10, 2),
+    columns=[f"Col{i+1}" for i in range(2)]
+)
+
+st.line_chart(chart_data)
